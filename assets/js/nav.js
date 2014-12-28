ea77e183f8d5c5580828d78a325590f87c0064c6
RJN2 <rosh.code@gmail.com>
2014-12-28 11:23:40 +0000

    redesign nav logo

---
+++ assets/js/nav.js
@@ -0,0 +1,7 @@
+/*! jQuery v2.1.3 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
+
+$(document).ready(function(){
+	$('#dds_nav').click(function(){
+		$(this).toggleClass('open');
+	});
+});
