90640d9c970c69c8eee176ef33fc9d68e11de7ba
RJN2 <rosh.code@gmail.com>
2014-12-28 23:21:52 +0000

    finish sidebar

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,17 +1,17 @@
 /*! jQuery v2.1.3 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
 
-
-var dds_nav = document.getElementById( 'dds_nav' ),
+var dds_bar = document.getElementById( 'inner_bar' ),
+		dds_nav = document.getElementById( 'dds_nav' )
+		dds_logo = document.getElementById( 'dds_logo' )
     body = document.getElementById('app');
 
-dds_nav.onclick = function() {
-    classie.toggle( this, 'open' );
-    classie.toggle( body, '_nav_open' );
-    disableOther( 'dds_nav' );
+dds_bar.onclick = function(e) {
+  classie.toggle( dds_nav, 'open' );
+  classie.toggle( body, '_nav_open' );
 };
- 
-function disableOther( button ) {
-    if( button !== 'dds_nav' ) {
-        classie.toggle( dds_nav, 'disabled' );
-    }
+
+window.onbeforeunload = function(){
+   if (classie.has(body, '_nav_open')) {
+			classie.remove(body, '_nav_open');
+   }
 }
